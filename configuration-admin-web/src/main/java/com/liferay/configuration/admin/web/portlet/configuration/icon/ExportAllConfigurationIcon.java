3c7ce2fab439ee9b81918dc7868a082bccd9ef26
Brian Chan <brian.chan@liferay.com>
2016-03-08 11:47:44 -0800

    LPS-64031 rename modules/apps/platform to modules/apps/foundation part 1

---
+++ configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/portlet/configuration/icon/ExportAllConfigurationIcon.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.configuration.admin.web.portlet.configuration.icon;
+
+import com.liferay.configuration.admin.web.constants.ConfigurationAdminPortletKeys;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortletURL;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.ResourceBundle;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jorge Ferrer
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" +
+			ConfigurationAdminPortletKeys.SYSTEM_SETTINGS
+	},
+	service = PortletConfigurationIcon.class
+)
+public class ExportAllConfigurationIcon extends BasePortletConfigurationIcon {
+
+	@Override
+	public String getMessage(PortletRequest portletRequest) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", getLocale(portletRequest), getClass());
+
+		return LanguageUtil.get(resourceBundle, "export-all-settings");
+	}
+
+	@Override
+	public String getMethod() {
+		return "GET";
+	}
+
+	@Override
+	public String getURL(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		LiferayPortletURL liferayPortletURL =
+			(LiferayPortletURL)PortalUtil.getControlPanelPortletURL(
+				portletRequest, ConfigurationAdminPortletKeys.SYSTEM_SETTINGS,
+				PortletRequest.RESOURCE_PHASE);
+
+		liferayPortletURL.setResourceID("export");
+
+		return liferayPortletURL.toString();
+	}
+
+	@Override
+	public double getWeight() {
+		return 1;
+	}
+
+	@Override
+	public boolean isShow(PortletRequest portletRequest) {
+		return true;
+	}
+
+}
---
+++ configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/portlet/configuration/icon/ExportFactoryInstancesIcon.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.configuration.admin.web.portlet.configuration.icon;
+
+import com.liferay.configuration.admin.web.constants.ConfigurationAdminPortletKeys;
+import com.liferay.configuration.admin.web.constants.ConfigurationAdminWebKeys;
+import com.liferay.configuration.admin.web.model.ConfigurationModel;
+import com.liferay.configuration.admin.web.util.ConfigurationModelIterator;
+import com.liferay.portal.kernel.portlet.LiferayPortletURL;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.util.PortalUtil;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jorge Ferrer
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" +
+			ConfigurationAdminPortletKeys.SYSTEM_SETTINGS,
+		"path=/view_factory_instances"
+	},
+	service = PortletConfigurationIcon.class
+)
+public class ExportFactoryInstancesIcon extends BasePortletConfigurationIcon {
+
+	@Override
+	public String getMessage(PortletRequest portletRequest) {
+		return "export-entries";
+	}
+
+	@Override
+	public String getMethod() {
+		return "GET";
+	}
+
+	@Override
+	public String getURL(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		LiferayPortletURL liferayPortletURL =
+			(LiferayPortletURL)PortalUtil.getControlPanelPortletURL(
+				portletRequest, ConfigurationAdminPortletKeys.SYSTEM_SETTINGS,
+				PortletRequest.RESOURCE_PHASE);
+
+		ConfigurationModel factoryConfigurationModel =
+			(ConfigurationModel)portletRequest.getAttribute(
+				ConfigurationAdminWebKeys.FACTORY_CONFIGURATION_MODEL);
+
+		liferayPortletURL.setParameter(
+			"factoryPid", factoryConfigurationModel.getFactoryPid());
+
+		liferayPortletURL.setResourceID("export");
+
+		return liferayPortletURL.toString();
+	}
+
+	@Override
+	public double getWeight() {
+		return 1;
+	}
+
+	@Override
+	public boolean isShow(PortletRequest portletRequest) {
+		ConfigurationModelIterator configurationModelIterator =
+			(ConfigurationModelIterator)portletRequest.getAttribute(
+				ConfigurationAdminWebKeys.CONFIGURATION_MODEL_ITERATOR);
+
+		if (configurationModelIterator.getTotal() > 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+}
